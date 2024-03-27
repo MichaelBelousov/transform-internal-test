b30f75efa9f2485051b3d0358a50eb9e5fb45481
Michael Belousov <michael.belousov98@gmail.com>
2024-03-27 16:04:02 -0400

    attempt at actual function declaration rewriting

--- packages/transform/index.ts
+++ packages/transform/index.ts
@@ -1,5 +1,5 @@
-import * as ts from 'typescript';
-import type { TransformerExtras, PluginConfig } from 'ts-patch';
+import * as ts from "typescript";
+import type { TransformerExtras, PluginConfig } from "ts-patch";
 
 function getParentSymbolName(declaration: any) {
   if (declaration.parent && declaration.parent.symbol && !declaration.parent.symbol.escapedName.startsWith('"'))
@@ -9,58 +9,118 @@ function getParentSymbolName(declaration: any) {
 
 const bannedTags = ["internal"];
 
-function checkJsDoc(declaration: any, node: any) {
-  if (!declaration || !declaration.jsDoc)
-    return undefined;
-
-  for (const jsDoc of declaration.jsDoc) {
-    if (jsDoc.tags) {
-      for (const tag of jsDoc.tags) {
-        if (!bannedTags.includes(tag.tagName.escapedText)) {
-          continue;
-        }
-        let name;
-        if (declaration.kind === ts.SyntaxKind.Constructor)
-          name = declaration.parent.symbol.escapedName;
-        else {
-          name = declaration.symbol.escapedName;
-          const parentSymbol = getParentSymbolName(declaration);
-          if (parentSymbol)
-            name = `${parentSymbol}.${name}`;
-        }
-
-        console.log('NAME', name);
-        return true;
-      }
-    }
-  }
+export interface CustomOptions {
+  /**
+   * The string to prepend to APIs marked as '@internal'
+   * @default "__INTERNAL_" {@link defaultOptions}
+   */
+  internalPrefix: string;
+  // FIXME: not generic enough, the "warn" behavior should be a mutually exclusive option
+  /**
+   * The message to warn with when a consumer of the library uses an '@internal' API directly
+   * The string {{name}} will be replaced with the name of the used API.
+   * @default "{{name}} is an internal API and may change without notice" {@link defaultOptions}
+   */
+  messageTemplate: string;
 }
 
+export const defaultOptions: Required<CustomOptions> = {
+  internalPrefix: "__INTERNAL_",
+  messageTemplate: "{{name}} is an internal API and may change without notice",
+}
 
 /** Changes string literal 'before' to 'after' */
-export default function (program: ts.Program, pluginConfig: PluginConfig, { ts: tsInstance }: TransformerExtras) {
+export default function (
+  program: ts.Program,
+  pluginConfig: PluginConfig & CustomOptions,
+  { ts }: TransformerExtras
+) {
+  const options = { ...defaultOptions, ...pluginConfig };
+
+  function checkJsDoc(declaration: any): boolean {
+    if (!declaration || !declaration.jsDoc)
+      return false;
+
+    for (const jsDoc of declaration.jsDoc) {
+      if (jsDoc.tags) {
+        for (const tag of jsDoc.tags) {
+          if (!bannedTags.includes(tag.tagName.escapedText)) {
+            continue;
+          }
+          let name;
+          if (declaration.kind === ts.SyntaxKind.Constructor)
+            name = declaration.parent.symbol.escapedName;
+          else {
+            name = declaration.symbol.escapedName;
+            const parentSymbol = getParentSymbolName(declaration);
+            if (parentSymbol)
+              name = `${parentSymbol}.${name}`;
+          }
+
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
   return (ctx: ts.TransformationContext) => {
     const { factory } = ctx;
     const typeChecker = program.getTypeChecker();
 
     return (sourceFile: ts.SourceFile) => {
-      function visit(node: ts.Node): ts.Node {
-        if (tsInstance.isStringLiteral(node) && (node as any).text === 'before') {
-          return factory.createStringLiteral('after');
-        }
-        if (tsInstance.isCallExpression(node)) {
-          const resolved = typeChecker.getResolvedSignature(node as any);
-          //checkJsDoc(node, node);
-          //console.log(resolved)
+      function visit(node: ts.Node): ts.Node | ts.Node[] | undefined {
+        if (ts.isFunctionDeclaration(node)) {
+          const message = options.messageTemplate
+            .replace(/(?<!\\)\{\{name}}/, `${node.name}`);
+
+          return [
+            factory.createFunctionDeclaration(
+              node.modifiers,
+              undefined,
+              node.name ? `${options.internalPrefix}${node.name.text}` : undefined,
+              node.typeParameters,
+              node.parameters,
+              node.type,
+              node.body,
+            ),
+            factory.createFunctionDeclaration(
+              node.modifiers,
+              undefined,
+              node.name ? `${options.internalPrefix}${node.name.text}` : undefined,
+              node.typeParameters,
+              node.parameters,
+              node.type,
+              factory.createBlock([
+                factory.createExpressionStatement(
+                  factory.createCallExpression(
+                    factory.createPropertyAccessExpression(
+                      factory.createIdentifier("console"),
+                      factory.createIdentifier("warn"),
+                    ),
+                    undefined,
+                    [
+                      // FIXME: better template handling
+                      factory.createStringLiteral(message)
+                    ],
+                  )
+                )
+              ]),
+            ),
+          ];
+        } else if (ts.isIdentifier(node)) {
+          const resolved = typeChecker.getSymbolAtLocation(node);
           if (resolved) {
-            checkJsDoc(resolved.declaration, node);
-            // return factory.createCallExpression(node.expression, undefined, node.arguments);
-            return factory.createCallExpression(factory.createIdentifier('__INTERNAL_'+(node.expression as any).text), undefined, node.arguments);
+            if (checkJsDoc(resolved.valueDeclaration))
+              return factory.createIdentifier(`__INTERNAL_${node.text}`);
           }
         }
-        return tsInstance.visitEachChild(node, visit, ctx);
+
+        return ts.visitEachChild(node, visit, ctx);
       }
-      return tsInstance.visitNode(sourceFile, visit);
+
+      return ts.visitNode(sourceFile, visit);
     };
   };
 }
