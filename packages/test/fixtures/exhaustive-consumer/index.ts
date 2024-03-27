d25fe19565527c67f9e43de216ccf906f15277ab
Michael Belousov <michael.belousov98@gmail.com>
2024-03-26 21:15:56 -0400

    man exhaustive is exhausting

--- packages/test/fixtures/exhaustive-consumer/index.ts
+++ packages/test/fixtures/exhaustive-consumer/index.ts
@@ -1,13 +1,3 @@
-/// declarations ////////////////
+import { internalFunction } from "exhaustive-pkg";
 
-/** @internal */
-function internal() {}
-/** @internal */
-const internalVar = 'before'
-/** @internal */
-class Internal {}
-
-/// local usage (should be transformed) ////////////////
-internal();
-console.log(internalVar);
-class Bad extends Internal {}
+internalFunction();
--- packages/test/fixtures/exhaustive-pkg/exhaustive.ts
+++ packages/test/fixtures/exhaustive-pkg/exhaustive.ts
@@ -1,13 +1,51 @@
-/// declarations ////////////////
+/** @internal */
+export function internalFunction() {}
+
+internalFunction();
 
 /** @internal */
-function internal() {}
-/** @internal */
-const internalVar = 'before'
-/** @internal */
-class Internal {}
+export const internalVar = 'internal';
 
-/// local usage (should be transformed) ////////////////
-internal();
-console.log(internalVar);
-class Bad extends Internal {}
+(globalThis as any)._test = internalVar;
+
+/** @internal */
+export class InternalClass {}
+
+new InternalClass();
+
+/** @public */
+export class PublicClass {
+  /** @internal */
+  public internalMethod() {}
+  /** @internal */
+  public get internalProperty() { return 2; }
+  /** @internal */
+  public set internalProperty(_: number) {}
+  /** @internal constructor */
+  public constructor() {}
+}
+
+new PublicClass().internalMethod();
+(globalThis as any)._publicClass_internalProperty = new PublicClass().internalProperty;
+new PublicClass().internalProperty = 3;
+
+/** @internal */
+export interface InternalInterface {}
+
+const interfaceInst: InternalInterface = {};
+
+/** @internal */
+export type InternalType = {}
+
+const typeInst: InternalType = {};
+
+/** @internal */
+export enum InternalEnum {}
+
+/** @public */
+export enum PublicEnum {
+  /** @public */
+  publicEnumMember = 1,
+  /** @internal */
+  internalEnumMember = 2,
+}
